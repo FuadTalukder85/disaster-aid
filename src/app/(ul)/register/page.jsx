746b66c07e12a393555d44a65cb08c6c7d215c16
Fuad Talukder <webfuadtk@gmail.com>
2025-03-11 15:02:35 +0600

    setup boiler plate

---
+++ src/app/(ul)/register/page.jsx
@@ -0,0 +1,8 @@
+import Container from "@/components/Container";
+import React from "react";
+
+const Register = () => {
+  return <Container className="text-red-500">This is register page</Container>;
+};
+
+export default Register;
