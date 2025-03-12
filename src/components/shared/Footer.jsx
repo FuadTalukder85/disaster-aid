6543429f84cd9d57038b000841f2e7a0d4786116
Fuad Talukder <webfuadtk@gmail.com>
2025-03-12 12:58:56 +0600

    make header and footer

--- src/components/shared/Footer.jsx
+++ src/components/shared/Footer.jsx
@@ -1,7 +1,80 @@
-import React from "react";
+import { FaEnvelope } from "react-icons/fa";
+import { IoIosCall } from "react-icons/io";
+import { FaFacebook } from "react-icons/fa";
+import { FaInstagram } from "react-icons/fa";
+import { FaTwitter } from "react-icons/fa";
+import { SlSocialPintarest } from "react-icons/sl";
+import Container from "../Container";
+import logo from "../../../public/disaster-aid-02.png";
+import Image from "next/image";
 
 const Footer = () => {
-  return <div>This is footer</div>;
+  return (
+    <div className="bg-primary">
+      <Container>
+        <footer className="grid grid-cols-12 py-16">
+          <div className="col-span-4">
+            <Image src={logo} alt="logo"></Image>
+            <p className="max-w-[34ch] mt-3 text-white">
+              Nulla ultricies justo sit amet ante efficitur, eget pharetra augue
+              efficitur. Vestibulum viverra, dolor sit amet ultricies.
+            </p>
+            <button className="bg-red px-6 py-2 mt-6 text-white uppercase font-semibold focus:outline-none">
+              Donate Now
+            </button>
+          </div>
+          <div className="col-span-2 text-white ms-10">
+            <h5 className="text-xl font-semibold">Service</h5>
+            <ul className="mt-5 space-y-2">
+              <li>Donate</li>
+              <li>Sponsor</li>
+              <li>Fundraise</li>
+              <li>Volunteer</li>
+              <li>Partner</li>
+            </ul>
+          </div>
+          <div className="col-span-3 text-white">
+            <h5 className="text-xl font-semibold">Non profit</h5>
+            <ul className="mt-5 space-y-2">
+              <li>Differently Abled Kids</li>
+              <li>Help Child Cancer</li>
+              <li>Clean Pure Water</li>
+              <li>Give them Education</li>
+              <li>Start a Fundraising</li>
+            </ul>
+          </div>
+          <div className="col-span-3 text-white">
+            <h5 className="text-xl font-semibold">Contact</h5>
+            <p className="mt-2">
+              380 Street Kilda Broklyn Road Melbourne Australia
+            </p>
+            <div className="flex gap-3 items-center mt-2">
+              <FaEnvelope className="text-white text-xl"></FaEnvelope>
+              disaster@gmail.com
+            </div>
+            <div className="flex gap-3 items-center">
+              <IoIosCall className="text-white text-2xl"></IoIosCall>+ 98 (000)
+              - 9630
+            </div>
+            <p className="mt-2 flex gap-3">
+              <button className="border-2 border-red w-[40px] h-[40px] flex items-center justify-center rounded-full">
+                <FaFacebook className="text-white text-xl"></FaFacebook>
+              </button>
+              <button className="border-2 border-red w-[40px] h-[40px] flex items-center justify-center rounded-full">
+                <FaInstagram className="text-white text-xl"></FaInstagram>
+              </button>
+              <button className="border-2 border-red w-[40px] h-[40px] flex items-center justify-center rounded-full">
+                <FaTwitter className="text-white text-xl"></FaTwitter>
+              </button>
+              <button className="border-2 border-red w-[40px] h-[40px] flex items-center justify-center rounded-full">
+                <SlSocialPintarest className="text-white text-xl"></SlSocialPintarest>
+              </button>
+            </p>
+          </div>
+        </footer>
+      </Container>
+    </div>
+  );
 };
 
 export default Footer;
--- src/components/shared/Nav.jsx
+++ src/components/shared/Nav.jsx
@@ -1,7 +1,28 @@
-import React from "react";
-
+import Image from "next/image";
+import logo from "../../../public/disaster-aid.png";
+import Container from "../Container";
+const menus = ["Home", "Donation", "Crisis", "Volunteer", "Inventory"];
 const Nav = () => {
-  return <div>This is nav</div>;
+  return (
+    <Container>
+      <div className="flex justify-between items-center py-5">
+        <Image src={logo} alt="logo" width={200}></Image>
+        <div>
+          <ul className="flex justify-between items-center gap-5 text-primary font-semibold">
+            {menus?.map((menu, index) => (
+              <li key={index}>{menu}</li>
+            ))}
+          </ul>
+        </div>
+        <div>
+          <ul className="flex justify-between items-center text-primary font-semibold gap-5">
+            <li>Login</li>
+            <li>Register</li>
+          </ul>
+        </div>
+      </div>
+    </Container>
+  );
 };
 
 export default Nav;
